c7a908cf487d2641362ed2e3cbaa68770b30cb96
harsh kumar <harshkumar939392@gmail.com>
2026-07-21 21:55:03 +0530

    initial commit

---
+++ simple_age_calcultor.py
@@ -0,0 +1,14 @@
+print("======================================")
+print("           simple calculator          ")
+print("======================================")
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+addition= num1+num2
+substraction= num1-num2
+multiplication= num1*num2
+division= num1/num2
+print("addition      :", addition)
+print("substraction  :", substraction)
+print("multiplication:", multiplication)
+print("division      :", division)
+print("========================================")
